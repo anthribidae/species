34fd610d2175980e110231c80f59eebdd8ef764d
Samanta Orellana <kasaore@gmail.com>
2025-08-17 16:28:19 -0500

    Update SectionDescription.vue

--- pages/components/Section/SectionDescription.vue
+++ pages/components/Section/SectionDescription.vue
@@ -4,22 +4,26 @@
       <div class="prose !container dark:prose-invert">
         <div>
           <p>
-            The Orthoptera Species File (OSF) is a taxonomic database of the
-            world’s Orthoptera (grasshoppers, locusts, katydids, crickets, and
-            related insects) both living and fossil. It has full taxonomic and
-            synonymic information for all taxa, with complete taxonomic
-            references, images, sound recordings, and specimen records.
+            Welcome to **Anthribidae of the World Online**, a virtual catalog that aims to facilitate the search of fungus weevil taxa. The taxonomic database is actively fed on [TaxonWorks](https://taxonworks.org) following specialized scientific literature. Specimen records and images are the product of the curation, identification, imaging and digitization of Anthribidae shared by several collections.   
           </p>
           <p>
-            The core purpose of the OSF is to provide a resource that makes
-            research on Orthoptera more efficient as well as to cover the needs
-            of policy-makers, environmental managers and the wider public for a
-            consistent and up-to-date classification of the world’s Orthoptera
-            species.
+            The following collections have facilitated access to Anthribidae specimens through visits or loans:
+
+* **AMNH**   American Museum of Natural History, USA (Dr. Lee Herman)
+* **ASUCOB**  O'Brien Collection, Arizona State University, USA (Dr. Sangmi Lee, Dr. Nico Franz)
+* **ASUHIC**  Hasbrouck Insect Collection, Arizona State University, USA (Dr. Sangmi Lee, Dr. Nico Franz)
+* **KUNHM**   Kansas University Natural Histroy Museum, USA (Dr. Zack Falin)
+* **NMNH**    Smithsonian National Museum of Natural History, USA (Dr. Lourdes Chamorro)
+* **NZAC**    New Zealand Arthropod Collection, New Zealand (Dr. Richard Leschen, Grace Hall)
+* **SSAC**    Salvatore S. Anzaldo Research Collection, USA (Dr. Salvatore Anzaldo)
           </p>
           <p>
-            It is open to contributions from anyone with specimen records and
-            images.
+            ## Announcements
+* **2024-04-30** - Udates in preparation for [TaxonWorks Together 2024](https://together.taxonworks.org/)!
+* **2024-02-05** - Publication of an updated checklist of Anthribidae of Colombia ([Orellana & Girón 2024](https://revistas.humboldt.org.co/index.php/biota/article/view/1149)) with a reference to this catalog!
+* **2023-11-29** - This resource is a work in progress! Current focus is on Neotropical species and type genera of the different tribes. Complete genera: **_Eugonodes_** Jordan, **_Eugonus_** Schoenherr, **_Phaenithon_** Schoenherr, **_Corrhecerus_** Schoenherr, **_Nemotrichus_** Labram & Imhoff, **_Discotenes_** Labram & Imhoff, **_Eucyclotropis_** Jordan, **_Trachytropis_** Jordan.
+* **2023-10-25** - Our website is live!
+
           </p>
         </div>
       </div>
