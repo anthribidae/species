10f45c6728dc94a642127d15e167742d8eaf5ad2
Samanta Orellana <kasaore@gmail.com>
2025-08-17 10:22:20 -0500

    Update and rename panels/inaturalist to panels/iNaturalist/main.js

---
+++ panels/iNaturalist/main.js
@@ -0,0 +1,8 @@
+
+import PaneliNaturalist from './PaneliNaturalist.vue'
+
+export default {
+  id: 'panel:inaturalist',
+  component: PaneliNaturalist,
+  rankGroup: ['GenusGroup', 'SpeciesGroup']
+}
